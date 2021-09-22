c1b0ce4c919ac00d4596b0f2afcfbd8611035a6f
Viktor Moskalev <viktormoskalev07@gmail.com>
2021-09-22 13:19:40 +0300

    add page

---
+++ src/js/old.js
@@ -0,0 +1,120 @@
+(function($) {
+
+    $("section:not(.hero-banner) .robot > a").click(function(){
+        $(this).parent().toggleClass("active");
+        return false;
+	});	
+
+    $(".call-us-section .robot > a").click(function(){
+        $(this).parent().toggleClass("active");
+        return false;
+	});	
+    
+	$(".hero-banner .robot a").click(function () {
+		$(this).parent().toggleClass("act");
+    	return false;
+	})    
+    
+	$(window).on('load', function() {
+  		$(".hero-banner .robot").addClass("active");
+	})
+
+    // Services Details Slider
+    $('.how-work-slider').slick({
+        slidesToShow: 2,
+        infinite: true,
+        pauseOnFocus: false,
+        pauseOnHover: false,
+        pauseOnDotsHover: false,
+        slide: '.item',
+        arrows: true,
+        dots: false,
+        responsive: [{
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 1
+            }
+        }]
+    });
+
+    $(".typewriter").click(function() {
+        $(this).addClass("act");
+    });
+
+    $('a[href*="#seo"]').click(function() {
+        $(".seo-popup-wrap").addClass("act");
+        return false;
+    });    
+
+    $('a[href*="#popup"]').click(function() {
+        $(".step-popup-wrap").addClass("act");
+        return false;
+    });    
+
+    $('.popup-close').click(function() {
+        $(".seo-popup-wrap").removeClass("act");
+        $(".step-popup-wrap").removeClass("act");
+    });    
+
+    $('a[href*="#contact"]').click(function() {
+        $('html, body').animate({
+            scrollTop: $("#contacto").offset().top - 100
+        }, 900);
+        return false;
+    });
+
+    // Fixed Header
+    //$(window).scroll(function() {
+        //var value = $(this).scrollTop();
+        //if (value > 0)
+           // $("header").addClass("sticky");
+        //else
+           // $("header").removeClass("sticky");
+    //});
+
+    // Menu
+    $('.humburger-menu').on('click', function() {
+        $('.header-wrapper .navbar, .humburger-menu, body').toggleClass('active');
+    });
+    $('.humburger-menu').click(function() {
+        $('.header-wrapper .navbar').slideToggle('slow');
+    });
+    $(window).resize(function() {
+        if ($(window).width() > 991) {
+            $('.header-wrapper .navbar').removeAttr('style');
+            $('.humburger-menu').removeClass('active');
+        }
+    });
+
+    // Tabs
+    $(document).ready(function() {
+        $('.tab').click(function() {
+            $(".tab-content").removeClass('tab-active');
+            $(".tab-content[data-id='" + $(this).attr('data-id') + "']").addClass("tab-active");
+            $(".tab").removeClass('active');
+            $(this).parent().find(".tab").addClass('active');
+        });
+    });
+
+    // Accordian
+    $('.toggle').click(function(e) {
+        e.preventDefault();
+        let $this = $(this);
+
+        $('.toggle').not(this).removeClass('active');
+
+        $this.toggleClass('active');
+
+        if ($this.next().hasClass('show')) {
+            $this.next().removeClass('show');
+            $this.next().slideUp(350);
+        } else {
+            $this.parent().parent().find('li .inner').removeClass('show');
+            $this.parent().parent().find('li .inner').slideUp(350);
+            $this.next().toggleClass('show');
+            $this.next().slideToggle(350);
+        }
+    });
+
+
+})(jQuery);
